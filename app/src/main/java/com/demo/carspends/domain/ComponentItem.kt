8ec1f90721ff9b8144ff13a710afc3c0920215f8
keklik2 <63215768+keklik2@users.noreply.github.com>
2021-11-13 04:31:55 +0300

    Added domain layout classes and interfaces

---
+++ app/src/main/java/com/demo/carspends/domain/ComponentItem.kt
@@ -0,0 +1,9 @@
+package com.demo.carspends.domain
+
+data class ComponentItem (
+    val id: Int,
+    val title: String,
+    val startMileage: Int,
+    val resourceMileage: Int,
+    val date: Long
+    )
